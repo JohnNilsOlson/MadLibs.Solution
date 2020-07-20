a710a34435c333a8819241a7fc8470b86bca19ce
JohnNils Olson <johnnils@gmail.com>
2020-07-20 10:56:54 -0700

    add madlibs route
    Co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
@@ -6,5 +7,18 @@ namespace MadLibs.Controllers
   {
     [Route("/")]
     public ActionResult Form() { return View(); }
+
+    [Route("/madlib")]
+    public ActionResult MadLibs(string firstAdjective, string color, string firstAnimal, string verb, string secondAdjective, string secondAnimal)
+    {
+      MadLibsVariable newMadLibsVariable = new MadLibsVariable();
+      newMadLibsVariable.FirstAdjective = firstAdjective;
+      newMadLibsVariable.Color = color;
+      newMadLibsVariable.FirstAnimal = firstAnimal;
+      newMadLibsVariable.Verb = verb;
+      newMadLibsVariable.SecondAdjective = secondAdjective;
+      newMadLibsVariable.SecondAnimal = secondAnimal;
+      return View(newMadLibsVariable);
+    }
   }
 }
