9b874ed66353118ea48de81c7d49735353a63ab9
JohnNils Olson <johnnils@gmail.com>
2020-07-20 10:31:08 -0700

    add madlibsvariable model
    Co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ MadLibs/Models/MadLibsVariable.cs
@@ -0,0 +1,12 @@
+namespace MadLibs.Models
+{
+  public class MadLibsVariable
+  {
+    public string FirstAdjective {get; set;}
+    public string Color {get; set;}
+    public string FirstAnimal {get; set;}
+    public string Verb {get; set;}
+    public string SecondAdjective {get; set;}
+    public string SecondAnimal {get; set;}
+  }
+}
